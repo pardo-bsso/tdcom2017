751247e0b15dc0227d4ebb88a4ae92b914d60137
Adrián Pardini <adrian@camba.coop>
2017-06-08 21:48:37 -0300

    addnoise: fix amplitude, use Eb/N0

--- src/addnoise.m
+++ src/addnoise.m
@@ -1,11 +1,12 @@
 ## Assumes bit energy == 1
 function ret = addnoise (symbols, EbN0_db=3)
     EbN0 = 10 ^ (EbN0_db / 10);
+    amplitude = 1 / sqrt(EbN0);
 
     re = real(symbols);
     im = imag(symbols);
 
-    re = re + rand(1, length(re));
-    im = im + rand(1, length(im));
+    re = re + amplitude * randn(1, length(re));
+    im = im + amplitude * randn(1, length(im));
     ret = re + 1j * im;
 endfunction
