64641f8a9b044039ed88d39b7923bd19c294d406
Adrián Pardini <adrian@camba.coop>
2017-06-11 11:11:48 -0300

    addnoise: adds bit_per_symbol argument

--- src/addnoise.m
+++ src/addnoise.m
@@ -1,6 +1,7 @@
-## Assumes bit energy == 1
-function ret = addnoise (symbols, EbN0_db=3)
+## Assumes bit energy == 1 and one bit per symbol by default.
+function ret = addnoise (symbols, EbN0_db=3, bits_per_symbol=1)
     EbN0 = 10 ^ (EbN0_db / 10);
+    EbN0 = EbN0 * bits_per_symbol;
     amplitude = 1 / sqrt(EbN0);
 
     re = real(symbols);
