34b548a35ce93b6aba622b58a62689c1bdf19af1
Adrián Pardini <adrian@camba.coop>
2017-06-14 23:18:06 -0300

    qam16: use Gray encoding

--- src/qam16.m
+++ src/qam16.m
@@ -1,62 +1,54 @@
-## Bits: [ A B C D ]
-## [ A B ]: Quadrant
-## [ C D ]: Position within quadrant.
+## Given Bits: [ A B C D ]
 ##
-##        01          ^    11
+## Returns a QAM16 set of symbols with Gray mapping:
+## A B      I     C D      Q
+## 0 0     -3     0 0     -3
+## 0 1     -1     0 1     -1
+## 1 1     +1     1 1     +1
+## 1 0     +3     1 0     +3
+##
+##
+##                    ^
 ##                    |
-##        01  |  11   |    01  |  11
-##        ----+----   |    ----+----
-##        00  |  10   |    00  |  10
+##       0010 | 0110  |  1110 | 1010
+##       -----+-----  |  -----+-----
+##       0010 | 0111  |  1111 | 1011
 ##                    |
 ## -------------------|---------------->
 ##                    |
-##        00          |    10
-##                    |
-##        01  |  11   |    01  |  11
-##        ----+----   |    ----+----
-##        00  |  10   |    00  |  10
+##       0001 | 0101  |  1101 | 1001
+##       -----+-----  |  -----+-----
+##       0000 | 0100  |  1100 | 1000
 ##
 ## (energy per bit)
 function ret = qam16 (bitstream, energy=1)
     bitstream_size = length(bitstream);
 
+    if (mod(bitstream_size, 2) ~= 0)
+        bitstream(end+1) = 0;
+        bitstream_size = length(bitstream);
+    end
+
+    data_I = zeros(1, bitstream_size / 2);
+    data_Q = zeros(1, bitstream_size / 2);
+
     # Bits [A B]
     quad_odd  = bitstream(1:4:bitstream_size);
     quad_even = bitstream(2:4:bitstream_size);
+    data_I(1:2:end) = bitstream(1:4:end);
+    data_I(2:2:end) = bitstream(2:4:end);
 
     # Bits [C D]
-    pos_odd   = bitstream(3:4:bitstream_size);
-    pos_even  = bitstream(4:4:bitstream_size);
+    data_Q(1:2:end) = bitstream(3:4:end);
+    data_Q(2:2:end) = bitstream(4:4:end);
 
-    # Decompose process into a qpsk constellation offset by the quadrant position.
-    # That offset (center) is also another qpsk constellation.
-
-    # Make a temporary buffer, pad length to multiple of 2
-    size_quad = length(quad_odd) + length(quad_even);
-    size_quad = size_quad + mod(size_quad, 2);
-    bitstream_quad = zeros(1, size_quad);
-
-    bitstream_quad(1:2:size_quad) = quad_odd;
-    bitstream_quad(2:2:size_quad) = quad_even;
+    I = pam_gray(data_I);
+    Q = 1j * pam_gray(data_Q);
 
     # For 16QAM with symbols at -3A, -A, A and 3A the average symbol energy is (A^2) / 10
-    # So we want to have the new centers at +- 2/sqrt(10) and each quadrant constellation scaled by 1/sqrt(10)
-    # This way the average symbol energy is 1. After merging the two we scale again by sqrt(4) to make the Bit energy equal to 1.
+    # So we want each component scaled by 1/sqrt(10)
+    # This way the average symbol energy is 1. Then we scale again by sqrt(4) to make the Bit energy equal to 1.
 
-    offsets = qpsk(bitstream_quad);
-    offsets = (2/sqrt(10)) .* offsets;
-
-    # Make a temporary buffer, pad length to multiple of 2
-    size_data = length(pos_odd) + length(pos_even);
-    size_data = size_data + mod(size_data, 2);
-    bitstream_data = zeros(1, size_data);
-
-    bitstream_data(1:2:size_data) = pos_odd;
-    bitstream_data(2:2:size_data) = pos_even;
-
-    data = qpsk(bitstream_data);
-    data = (1/sqrt(10)) .* data;
-
-    ret = sqrt(4) .* (data + offsets);
+    ret = sqrt(4/10) * (I + Q);
 endfunction
 
